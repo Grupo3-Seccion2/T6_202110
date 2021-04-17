97537e3592c54955924f7e2cfb9b74ba9e200c9e
Daniel Acevedo <df.acevedo@uniandes.edu.co>
2021-04-17 17:59:35 -0500

    RBT

--- src/model/data_structures/NodoRBT.java
+++ src/model/data_structures/NodoRBT.java
@@ -2,86 +2,86 @@ package model.data_structures;
 
 public class NodoRBT<K extends Comparable<K>, V extends Comparable<V>> 
 {
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
-    boolean color;
-    protected K key;
-    public V value;
-    protected NodoRBT<K, V> left, right;
-    protected int size;
-    
-    public NodoRBT(K key, V value) 
-    {
-        this.key = key;
-        this.value = value;
-        this.size = 1;
-        this.color = false;
-    }
+	private static final boolean RED = true;
+	private static final boolean BLACK = false;
+	boolean color;
+	protected K key;
+	public V value;
+	protected NodoRBT<K, V> left, right;
+	protected int size;
 
-    public NodoRBT(K key, V value, boolean color) 
-    {
-        this.key = key;
-        this.value = value;
-        this.size = 1;
-        this.color = color;
-    }
+	public NodoRBT(K key, V value) 
+	{
+		this.key = key;
+		this.value = value;
+		this.size = 1;
+		this.color = false;
+	}
 
-    public NodoRBT<K, V> getLeft() 
-    {
-        return this.left;
-    }
+	public NodoRBT(K key, V value, boolean color) 
+	{
+		this.key = key;
+		this.value = value;
+		this.size = 1;
+		this.color = color;
+	}
 
-    public void setLeft(NodoRBT<K, V> left)
-    {
-        this.left = left;
-    }
+	public NodoRBT<K, V> getLeft() 
+	{
+		return this.left;
+	}
 
-    public NodoRBT<K, V> getRight() 
-    {
-        return this.right;
-    }
+	public void setLeft(NodoRBT<K, V> left)
+	{
+		this.left = left;
+	}
 
-    public void setRight(NodoRBT<K, V> right) 
-    {
-        this.right = right;
-    }
+	public NodoRBT<K, V> getRight() 
+	{
+		return this.right;
+	}
 
-    public K getKey() 
-    {
-        return this.key;
-    }
+	public void setRight(NodoRBT<K, V> right) 
+	{
+		this.right = right;
+	}
 
-    public V getValue() 
-    {
-        return this.value;
-    }
+	public K getKey() 
+	{
+		return this.key;
+	}
 
-    public void setValue(V value) 
-    {
-        this.value = value;
-    }
+	public V getValue() 
+	{
+		return this.value;
+	}
 
-    public boolean getColor() 
-    {
-        return isRed(this);
-    }
+	public void setValue(V value) 
+	{
+		this.value = value;
+	}
 
-    public void setColor(boolean newColor) 
-    {
-        this.color = (newColor == RED) ? RED : BLACK;
-    }
+	public boolean getColor() 
+	{
+		return isRed(this);
+	}
 
-    private boolean isRed(NodoRBT<K, V> node) 
-    {
-        if (node == null) return false;
-        return node.color == RED;
-    }
+	public void setColor(boolean newColor) 
+	{
+		this.color = (newColor == RED) ? RED : BLACK;
+	}
 
-    private boolean isBlack(NodoRBT<K, V> node)
-    {
-        if (node == null) return false;
-        return node.color == BLACK;
-    }
+	private boolean isRed(NodoRBT<K, V> node) 
+	{
+		if (node == null) return false;
+		return node.color == RED;
+	}
+
+	private boolean isBlack(NodoRBT<K, V> node)
+	{
+		if (node == null) return false;
+		return node.color == BLACK;
+	}
 
 
 }
